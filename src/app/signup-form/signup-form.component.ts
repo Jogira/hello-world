0481d768e2bbd7fc5f86b9a6df871c967fa17595
Jonathan <Zweitonegoismus@gmail.com>
2023-03-22 15:31:35 -0400

    Custom validation error using form control validators.

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsernameValidators } from './username.validators';
 
 @Component({
   selector: 'signup-form',
@@ -9,8 +10,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class SignupFormComponent {
   form = new FormGroup(
     {
-      username: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required)
+      username: new FormControl('', [Validators.required, Validators.minLength(5), UsernameValidators.cannotContainSpace]),
+      password: new FormControl('', Validators.required),
     }
   );
   get username() {
