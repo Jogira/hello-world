b640122851a509bf2b76a092904ba5eb78c99611
Jonathan <Zweitonegoismus@gmail.com>
2023-03-23 12:51:43 -0400

    Form array refactored.

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormControl } from '@angular/forms'
 
 @Component({
   selector: 'new-course-form',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-course-form.component.css']
 })
 export class NewCourseFormComponent implements OnInit {
+  form = new FormGroup({
+    topics: new FormArray([])
+  })
+
+
   categories = [
     { id: 1, name: 'Development' },
     { id: 2, name: 'Art' },
@@ -13,6 +19,16 @@ export class NewCourseFormComponent implements OnInit {
   ];
 
   constructor() { }
+
+  addTopic(topic: HTMLInputElement) {
+    // (this.form.get('topics') as FormArray).push(new FormControl(topic.value));
+    this.topics.push(new FormControl(topic.value));
+    topic.value = '';
+  }
+
+  get topics() {
+    return this.form.get('topics') as FormArray;
+  }
 
   submit(course: any) {
     console.log(course);
