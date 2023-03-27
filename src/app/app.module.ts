260afe6fd18b41b6be1ed39e8d2ff195b76bf088
Jonathan <Zweitonegoismus@gmail.com>
2023-03-27 15:11:24 -0400

    Routing initialized.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,7 @@
+import { NavbarComponent } from './navbar/navbar.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 import { GithubFollowersService } from './services/github-followers.service';
 import { GithubFollowersComponent } from './github-followers/github-followers.component';
 import { AppErrorHandler } from './common/app-error-handler';
@@ -25,6 +29,7 @@ import { NewCourseFormComponent } from './new-course-form/new-course-form.compon
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
 import { PostsComponent } from './posts/posts.component';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
@@ -42,7 +47,11 @@ import { PostsComponent } from './posts/posts.component';
     SignupFormComponent,
     ChangePasswordComponent,
     PostsComponent,
-    GithubFollowersComponent
+    GithubFollowersComponent,
+    HomeComponent,
+    NotFoundComponent,
+    GithubProfileComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -50,6 +59,13 @@ import { PostsComponent } from './posts/posts.component';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'followers/:username', component: GithubProfileComponent },
+      { path: 'followers', component: GithubFollowersComponent },
+      { path: 'posts', component: PostsComponent },
+      { path: '**', component: NotFoundComponent },
+    ])
   ],
   providers: [CoursesService, AuthorsService, GithubFollowersService, PostService, { provide: ErrorHandler, useClass: AppErrorHandler }],
   bootstrap: [AppComponent]
